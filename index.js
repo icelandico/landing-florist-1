a821bce7e93f155f298e3dc2388cc1f8e56fe74b
Michal Muszynski <hi@geodev.me>
2025-08-06 22:01:06 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,93 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const navLinks = document.querySelectorAll("#main-nav a");
+  const sections = [];
+
+  navLinks.forEach((link) => {
+    const href = link.getAttribute("href");
+    if (href && href.startsWith("#")) {
+      const section = document.querySelector(href);
+      if (section) {
+        sections.push(section);
+      }
+    }
+  });
+
+  const observerOptions = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.5,
+  };
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      const id = entry.target.getAttribute("id");
+      const navLink = document.querySelector(`a[href="#${id}"]`);
+
+      if (entry.isIntersecting) {
+        navLink.parentElement.classList.add("active-nav");
+      } else {
+        navLink.parentElement.classList.remove("active-nav");
+      }
+    });
+  }, observerOptions);
+
+  sections.forEach((section) => {
+    observer.observe(section);
+  });
+});
+
+class Carousel {
+  constructor(element) {
+    this.content = element.querySelector(".carousel-content");
+    this.elements = Array.from(this.content.children);
+    this.elementsLength = this.elements.length;
+    this.arrowLeft = element.querySelector(".carousel-arrow-left");
+    this.arrowRight = element.querySelector(".carousel-arrow-right");
+    this.activeElement = 0;
+    this.updateArrowStates();
+  }
+
+  activateElement(n) {
+    if (n < 0 || n >= this.elementsLength) {
+      return;
+    }
+    this.activeElement = n;
+
+    const elementToScrollTo = this.elements[n];
+
+    this.content.scrollTo({
+      left: elementToScrollTo.offsetLeft - this.content.offsetLeft,
+      behavior: "smooth",
+    });
+
+    this.updateArrowStates();
+  }
+
+  updateArrowStates() {
+    if (this.activeElement <= 0) {
+      this.arrowLeft.classList.add("inactive");
+    } else {
+      this.arrowLeft.classList.remove("inactive");
+    }
+
+    const visibleElements = 3;
+    if (this.activeElement >= this.elementsLength - visibleElements) {
+      this.arrowRight.classList.add("inactive");
+    } else {
+      this.arrowRight.classList.remove("inactive");
+    }
+  }
+
+  addEventListeners() {
+    this.arrowLeft.addEventListener("click", () => {
+      this.activateElement(this.activeElement - 1);
+    });
+
+    this.arrowRight.addEventListener("click", () => {
+      this.activateElement(this.activeElement + 1);
+    });
+  }
+}
+
+const carousel = document.getElementById("carousel");
+new Carousel(carousel).addEventListeners();
